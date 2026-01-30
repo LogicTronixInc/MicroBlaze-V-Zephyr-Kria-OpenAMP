9befc7c0258bdc5611f3406ae430125ed5749a8b
krishnagaihre <gaihrekrishna@gmail.com>
2026-01-30 15:10:19 +0545

    VIVADO and Zephyr Project files-2

---
+++ Hardware_VIVADO/all_sensors_hardware_baremetal_test/microblaze_ddr_access_tested.c
@@ -0,0 +1,3 @@
+
+//Baremetal test of all_sensors_hardware - checking the memory test only to confirm the platform functionaloty
+// Update the ldscript and according to that use the memory address here.
