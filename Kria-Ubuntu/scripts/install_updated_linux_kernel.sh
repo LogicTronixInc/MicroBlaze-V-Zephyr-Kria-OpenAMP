32b80a858783fd0654ac2d2a91159445d5113c2d
sanamshakya <sanam_shakya@yahoo.com>
2026-02-03 18:59:58 +0545

    Updated Kria Ubuntu install linux kernel script

--- Kria-Ubuntu/scripts/install_updated_linux_kernel.sh
+++ Kria-Ubuntu/scripts/install_updated_linux_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget <>
+wget https://logictronix.com/wp-content/uploads/2026/02/kria-ubuntu-kernel-build-1027.zip
 unzip kria-ubuntu-kernel-build-1027.zip
-cd kernel_build_1027
+cd kria-ubuntu-kernel-build-1027
 sudo dpkg -i *.deb
 
 #bash install.sh
